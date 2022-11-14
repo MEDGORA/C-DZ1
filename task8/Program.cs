224cfdda0b599fcb56db3c46447bbd9ec56bedd2
Boris Dovbysh <boris.dovbysh@gmail.com>
2022-11-14 16:31:50 +0300

    Сделяль

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,28 +1,20 @@
-﻿Console.Write("Введите число: ");
+﻿Console.Write("Введите число (N): ");
 int N = Convert.ToInt32(Console.ReadLine());
 if (N < 2) 
 {
-    Console.WriteLine("Программа показывает только положительные чётные числа от 1");
+    Console.WriteLine("Программа показывает только положительные чётные числа от 1 до N, где N больше или равно 2");
 }
 else
 {
     for (int i = 2; i < N+1; i=i+2)
     {
-        //Console.Write($"{i}, ");
-        string a = ($"{i}, ");
-        //a = a.Substring(0, a.Length - 2);
-        //a -=", ";
-        string b = a;
-        //Console.Write(b.Substring(0, b.Length - 2));
-        Console.Write(b);
-        /*
-        string str = i.ToString();
-        str = str.Substring(str.Length - 2);
-        Console.Write(str);
-        /*
-        string text = Write($"{i}, ");
-        text = text.Substring(0, text.Length - 2);
-        Console.WriteLine(text);
-        */
+        if (i < N-1)
+        {
+        Console.Write($"{i}, ");
+        }
+        else
+        {
+        Console.Write(i);
+        }
     }
-}
+}    
